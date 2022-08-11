ecaac81ba081fc9394e7f9ad8ccf9d5ebad7d43b
Yameen Dasadiya <110965100+yameen-dasadiya@users.noreply.github.com>
2022-08-11 12:43:30 +0530

    Initial commit

---
+++ function_2.py
@@ -0,0 +1,26 @@
+def ft_and_inch_to_m(ft, inch):
+    return ft * 0.3048 + inch * 0.0254
+
+
+print(ft_and_inch_to_m(1, 1))
+
+
+def bmi(weight, height):
+    if height < 1.0 or height > 2.5 or \
+            weight < 20 or weight > 200:
+        return None
+
+    return weight / height ** 2
+
+
+print(bmi(352.5, 1.65))
+
+
+def is_a_triangle(a, b, c):
+    if a + b <= c or b + c <= a or c + a <= b:
+        return False
+    return True
+
+
+print(is_a_triangle(1, 1, 1))
+print(is_a_triangle(1, 1, 3))
---
+++ tony_stark.py
@@ -0,0 +1,9 @@
+fname = input("Enter First name : ")
+lname = input("Enter Last name : ")
+age = int(input("Enter Age : "))
+genius = True
+if fname == "Tony" and lname == "Stark":
+    print(fname + lname + " is Genius!")
+else:
+    print(fname + lname + " not genius")
+
